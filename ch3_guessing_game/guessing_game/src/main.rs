4884f7ca1c85158cc98656848eaa35052a12a6a3
Johnson Ko <komengchu@gmail.com>
2023-12-18 21:33:35 +0800

    read from stdio

--- ch3_guessing_game/guessing_game/src/main.rs
+++ ch3_guessing_game/guessing_game/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please guess a number:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Fail to read!");
+
+    println!("guess = {}", guess);
 }
