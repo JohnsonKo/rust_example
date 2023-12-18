a862fe14e6d56e2077cc8bf2616267ea116deebf
Johnson Ko <komengchu@gmail.com>
2023-12-18 21:59:55 +0800

    loop compare with secret number

--- ch3_guessing_game/guessing_game/src/main.rs
+++ ch3_guessing_game/guessing_game/src/main.rs
@@ -1,21 +1,38 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    println!("Please guess a number:");
+    let secret_number = rand::thread_rng().gen_range(1, 50);
 
-    let mut guess = String::new();
+    println!("secret number is {}", secret_number);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Fail to read!");
+    loop{
+        println!("Please guess a number:");
 
-    let guess: u32 = match guess.trim().parse(){
-        Ok(t) => t,
-        Err(e) => {
-            println!("error {e}");
-            continue;
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Fail to read!");
+    
+        let guess: u32 = match guess.trim().parse(){
+            Ok(t) => t,
+            Err(e) => {
+                println!("error {e}");
+                continue;
+            }
+        };
+
+        // println!("guess = {:0}", guess);
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
-    };
-
-    println!("guess = {:0}", guess);
+    }
 }
